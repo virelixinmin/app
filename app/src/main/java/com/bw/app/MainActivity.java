000b1fb54bd3dc098356c16e97fbbd9d80ff493b
virelixinmin <1648209074@qq.com>
2018-12-25 22:28:21 +0800

    xiugai

--- app/src/main/java/com/bw/app/MainActivity.java
+++ app/src/main/java/com/bw/app/MainActivity.java
@@ -2,6 +2,9 @@ package com.bw.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        class MyAdapyer extends BaseAdapter {
+
+            @Override
+            public int getCount() {
+                return 0;
+            }
+
+            @Override
+            public Object getItem(int i) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int i, View view, ViewGroup viewGroup) {
+                return null;
+            }
+        }
+    }}
